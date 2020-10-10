be7d7f3d98a20c816b9d3b07afb3d564092181de
Imran Sihab <57612580+imransihab0@users.noreply.github.com>
2020-10-11 00:29:39 +0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,24 @@
+function fn(){
+ document.querySelector("body").style.backgroundColor = "#a203ff";
+ }
+ 
+function fn1(){
+ document.querySelector("body").style.backgroundColor = "#f007dc";
+ }
+
+ 
+function fn2(){
+ document.querySelector("body").style.backgroundColor = " 	#ff1500";
+ }
+ 
+function fn3(){
+	document.querySelector("body").style.backgroundColor = "#03fc03";
+}
+
+function fn4(){
+	document.querySelector("body").style.backgroundColor = "#fcf803";
+}
+
+function fn5(){
+	document.querySelector("body").style.backgroundColor = "#8a8a84";
+}
